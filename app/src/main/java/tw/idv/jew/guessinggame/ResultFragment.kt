1fac49c78f2fde522ff36a7a9c77934133c26fa8
a0916105 <whdofjew@gmail.com>
2023-01-07 10:44:40 +0800

    ResultFragment

--- app/src/main/java/tw/idv/jew/guessinggame/ResultFragment.kt
+++ app/src/main/java/tw/idv/jew/guessinggame/ResultFragment.kt
@@ -5,12 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import tw.idv.jew.guessinggame.databinding.FragmentResultBinding
 
 class ResultFragment : Fragment() {
     private var _binding: FragmentResultBinding? = null
     private val binding get() = _binding!!
+
+    lateinit var viewModel: ResultViewModel
+    lateinit var viewModelFactory: ResultViewModelFactory
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,7 +24,16 @@ class ResultFragment : Fragment() {
         _binding = FragmentResultBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        binding.wonLost.text = ResultFragmentArgs.fromBundle(requireArguments()).result //資料存在Bundle，所以wonLost.text不會因畫面旋轉遺失
+        val result = ResultFragmentArgs.fromBundle(requireArguments()).result
+
+        //建立view model factory物件
+        viewModelFactory = ResultViewModelFactory(result)
+        //將view model factory傳給ViewModelProvider
+        viewModel = ViewModelProvider(this, viewModelFactory)
+            //如果還沒view model物件，ViewModelProvider會使用view model factory來建立view model
+            .get(ResultViewModel::class.java)
+
+        binding.wonLost.text = viewModel.result //改由viewModel來取得result，之前是從Bundle
 
         binding.newGameButton.setOnClickListener {
             view.findNavController()
