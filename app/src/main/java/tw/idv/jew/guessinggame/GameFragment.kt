60c165fb2f225103ab35ccead5b2206ae64725e8
a0916105 <whdofjew@gmail.com>
2023-01-07 15:42:28 +0800

    在GameFragment使用MutableLiveData

--- app/src/main/java/tw/idv/jew/guessinggame/GameFragment.kt
+++ app/src/main/java/tw/idv/jew/guessinggame/GameFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import tw.idv.jew.guessinggame.databinding.FragmentGameBinding
@@ -25,12 +26,21 @@ class GameFragment : Fragment() {
 
         viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
-        updateScreen()  //更新畫面
+        //更新畫面
+        viewModel.incorrectGuesses.observe(viewLifecycleOwner, Observer { newValue ->
+            binding.incorrectGuesses.text = "Incorrect guesses: $newValue"
+        })
+        viewModel.livesLeft.observe(viewLifecycleOwner, Observer { newValue ->
+            binding.lives.text = "You have $newValue lives left."
+        })
+        viewModel.secretWordDisplay.observe(viewLifecycleOwner, Observer { newValue ->
+            binding.word.text = newValue
+        })
 
         binding.guessButton.setOnClickListener {
             viewModel.makeGuess(binding.guess.text.toString().uppercase())
             binding.guess.text = null   //重設edit text
-            updateScreen()  //更新畫面
+
             if (viewModel.isWon() || viewModel.isLost()) {  //在用戶贏或輸時，前往ResultFragment，並將wonLostMessage()的回傳值傳給它
                 val action = GameFragmentDirections
                     .actionGameFragmentToResultFragment(viewModel.wonLostMessage())
@@ -44,12 +54,5 @@ class GameFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    //設定layout的text view
-    private fun updateScreen() {
-        binding.word.text = viewModel.secretWordDisplay
-        binding.lives.text = "You have ${viewModel.livesLeft} lives left."
-        binding.incorrectGuesses.text = "Incorrect guesses: ${viewModel.incorrectGuesses}"
     }
 }
