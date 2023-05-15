a86e20311d26793f17d21886c3f26426ede1c0e6
a0916105 <whdofjew@gmail.com>
2023-05-15 16:17:20 +0800

    移除ResultFragment的view

--- app/src/main/java/tw/idv/jew/guessinggame/ResultFragment.kt
+++ app/src/main/java/tw/idv/jew/guessinggame/ResultFragment.kt
@@ -13,17 +13,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
-import tw.idv.jew.guessinggame.databinding.FragmentResultBinding
 
 class ResultFragment : Fragment() {
-    private var _binding: FragmentResultBinding? = null
-    private val binding get() = _binding!!
-
     lateinit var viewModel: ResultViewModel
     lateinit var viewModelFactory: ResultViewModelFactory
 
@@ -31,10 +28,17 @@ class ResultFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        _binding = FragmentResultBinding.inflate(inflater, container, false).apply {    //使用apply將composable加入UI
+        val result = ResultFragmentArgs.fromBundle(requireArguments()).result
+        //建立view model factory物件
+        viewModelFactory = ResultViewModelFactory(result)
+        //將view model factory傳給ViewModelProvider
+        viewModel = ViewModelProvider(this@ResultFragment, viewModelFactory)
+            //如果還沒view model物件，ViewModelProvider會使用view model factory來建立view model
+            .get(ResultViewModel::class.java)
+        //先建立view model再回傳ComposeView
+        return ComposeView(requireContext()).apply {
             //告訴ComposeView要加入哪些composable
-            composeView.setContent {
+            setContent {    //移除composeView參考，因為它引用layout檔裡面的ComposeView
                 //將ResultFragmentContent加入layout的composeView，並套用預設的Material佈景主題
                 MaterialTheme {
                     Surface {
@@ -44,30 +48,6 @@ class ResultFragment : Fragment() {
                 }
             }
         }
-        val view = binding.root
-
-        val result = ResultFragmentArgs.fromBundle(requireArguments()).result
-
-        //建立view model factory物件
-        viewModelFactory = ResultViewModelFactory(result)
-        //將view model factory傳給ViewModelProvider
-        viewModel = ViewModelProvider(this, viewModelFactory)
-            //如果還沒view model物件，ViewModelProvider會使用view model factory來建立view model
-            .get(ResultViewModel::class.java)
-
-        binding.resultViewModel = viewModel //改用data binding（設定layout的data binding變數）
-
-        binding.newGameButton.setOnClickListener {
-            view.findNavController()
-                .navigate(R.id.action_resultFragment_to_gameFragment)
-        }
-
-        return view
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
 
