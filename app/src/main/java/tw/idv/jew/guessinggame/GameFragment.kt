f3bcc65fa7975aba5962b2b80b623dd27525e203
a0916105 <whdofjew@gmail.com>
2023-05-18 15:37:15 +0800

    建立SecretWordDisplay和LivesLeftText composable函式

--- app/src/main/java/tw/idv/jew/guessinggame/GameFragment.kt
+++ app/src/main/java/tw/idv/jew/guessinggame/GameFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
@@ -15,6 +17,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
@@ -72,6 +76,24 @@ class GameFragment : Fragment() {
 }
 
 @Composable
+fun SecretWordDisplay(viewModel: GameViewModel) {
+    val display = viewModel.secretWordDisplay.observeAsState()
+    display.value?.let {
+        Text(text = it,
+            letterSpacing = 0.1.em,
+            fontSize = 36.sp)
+    }
+}
+
+@Composable
+fun LivesLeftText(viewModel: GameViewModel) {
+    val livesLeft = viewModel.livesLeft.observeAsState()
+    livesLeft.value?.let {
+        Text(stringResource(R.string.lives_left, it))
+    }
+}
+
+@Composable
 fun IncorrectGuessesText(viewModel: GameViewModel) {
     //使用observeAsState()來回應LiveData
     val incorrectGuesses = viewModel.incorrectGuesses.observeAsState()
@@ -109,6 +131,13 @@ fun GameFragmentContent(viewModel: GameViewModel) {
     val guess = remember { mutableStateOf("") }
 
     Column(modifier = Modifier.fillMaxWidth()) {
+        Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center) {
+            SecretWordDisplay(viewModel)
+        }
+
+        LivesLeftText(viewModel)
+
         IncorrectGuessesText(viewModel)
 
         EnterGuess(guess.value ) { guess.value = it }
